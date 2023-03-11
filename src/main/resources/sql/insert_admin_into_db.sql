18b3f45605a26ae5c34dd7e08c1e1ef107548260
skostia91 <121064256+skostia91@users.noreply.github.com>
2023-03-12 02:13:24 +0300

    Update insert_admin_into_db.sql

--- src/main/resources/sql/insert_admin_into_db.sql
+++ src/main/resources/sql/insert_admin_into_db.sql
@@ -1,1 +1,1 @@
-insert into my_db.security set name = 'sidorov', year = 1986, password = '$12$NMeAo9x0KBmWVy.QOPtLKObZzqEGtbYv4Bb8C0UV.vVg97jPQxKNW', role = 'ROLE_ADMIN';
+insert into my_db.security set name = 'sidorov', year = 1986, password = '$2a$10$54Ug6oZe8NIKi5NaInPDiOSz6Vnj6cA2EXazkhV2tebRQXF8Xiacq', role = 'ROLE_ADMIN';
